dd6a2174ad43e48512df260e4686e2812dae926b
y-tikhenkiy <y.tikhenkiy@gmail.com>
2025-02-26 21:19:31 +0100

    modify API response example

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -3,9 +3,19 @@ import tags from '../test-data/tags.json'
 
 test.beforeEach(async ({page})=>{
     await page.route('*/**/api/tags', async route => {
-   
+      await route.fulfill({
+        body: JSON.stringify(tags)
+      })
+  })
+
+  await page.route('*/**/api/articles*', async route =>{
+    const response = await route.fetch();
+    const responseBody = await response.json();
+    responseBody.articles[0].title = "This is a test title";
+    responseBody.articles[0].description = "This is a test descriprion";
+    
     await route.fulfill({
-      body: JSON.stringify(tags)
+      body:JSON.stringify(responseBody)
     })
   })
 
@@ -14,6 +24,11 @@ test.beforeEach(async ({page})=>{
 
 })
 
-test('has title', async ({ page }) => {
+test('article has modified title and description', async ({ page }) => {
+
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title');
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a test descriprion');
+  
 });
